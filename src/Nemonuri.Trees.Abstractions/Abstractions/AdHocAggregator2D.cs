c2eea09730bbee7bbee2296596529076c693d2f4
nemonuri <nemonuri2@gmail.com>
2025-08-30 18:37:59 +0900

    csharp.code-snippets 추가 - 써 보니까, 그럭저럭 괜찮네?

---
+++ src/Nemonuri.Trees.Abstractions/Abstractions/AdHocAggregator2D.cs
@@ -0,0 +1,40 @@
+
+namespace Nemonuri.Trees.Abstractions;
+
+/// <inheritdoc cref="IAggregator2D{_,_}" path="/typeparam" />
+public readonly struct AdHocAggregator2D<TElement, TAggregation> :
+    IAggregator2D<TElement, TAggregation>
+#if NET9_0_OR_GREATER
+    where TElement : allows ref struct
+    where TAggregation : allows ref struct
+#endif
+{
+    private readonly Func<TAggregation> _initialAggregationImplementation;
+    private readonly Func<TAggregation, TAggregation, TElement, TAggregation> _aggregateImplementation;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AdHocAggregator2D{_,_}"/> structure with the specified implementations.
+    /// </summary>
+    /// <param name="initialAggregationImplementation">
+    /// The implementation of <see cref="IAggregator2D{_,_}.InitialAggregation"/>
+    /// </param>
+    /// <param name="aggregateImplementation">
+    /// The implementation of <see cref="IAggregator2D{_,_}.Aggregate(_,_,_)"/>
+    /// </param>
+    public AdHocAggregator2D
+    (
+        Func<TAggregation> initialAggregationImplementation,
+        Func<TAggregation, TAggregation, TElement, TAggregation> aggregateImplementation
+    )
+    {
+        _initialAggregationImplementation = initialAggregationImplementation;
+        _aggregateImplementation = aggregateImplementation;
+    }
+
+    /// <inheritdoc cref="IAggregator2D{_,_}.InitialAggregation" />
+    public TAggregation InitialAggregation => _initialAggregationImplementation();
+
+    /// <inheritdoc cref="IAggregator2D{_,_}.Aggregate(_,_,_)" />
+    public TAggregation Aggregate(TAggregation siblingsAggregation, TAggregation childrenAggregation, TElement element) =>
+        _aggregateImplementation(siblingsAggregation, childrenAggregation, element);
+}
