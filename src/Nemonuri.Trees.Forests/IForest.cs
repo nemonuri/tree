b3dce3fd094427c94eb5b779e060bba613d09164
nemonuri <nemonuri2@gmail.com>
2025-09-11 07:50:38 +0900

    forest 인터페이스 추가...으음

---
+++ src/Nemonuri.Trees.Forests/IForest.cs
@@ -0,0 +1,18 @@
+﻿namespace Nemonuri.Trees.Forests;
+
+public interface IForest<out TForest, out TTree, out TTreeChildren> :
+    ISupportChildren<TTreeChildren>
+    where TTree : ITree<TTree>
+    where TForest : IForest<TForest, TTree, TTreeChildren>
+    where TTreeChildren : IEnumerable<TTree>
+{
+}
+
+public interface IRoseForest<out TValue, out TForest, out TTree, out TTreeChildren> :
+    ISupportChildren<TTreeChildren>,
+    ISupportValue<TValue>
+    where TTree : ITree<TTree>
+    where TForest : IForest<TForest, TTree, TTreeChildren>
+    where TTreeChildren : IEnumerable<TTree>
+{
+}
